3d90bfd71ff170a1cfe8a5536ec4e04c1341732d
Michael Chary <mac389@gmail.com>
2014-06-29 17:05:20 -0400

    forgot to include one category of states in heatmap

--- src/legality-vs-states.py
+++ src/legality-vs-states.py
@@ -15,9 +15,11 @@ data = json.load(open('./data/word-occurences-by-category.json',READ))
 def jaccard(one,two):
 	return len(set(one) & set(two))/float(len(set(one) | set(two)))
 
-categories = ['legal','Medical','decriminalization','illegal']
+categories = ['legal','Medical','decriminalization','Medical-decriminalization','illegal']
 similarities = [[jaccard(first_category,second_category) for first_category in categories] 
 														 for second_category in categories]
+
+np.savetxt('./results/jaccard-similarity-marijuana.tsv',np.array(similarities),fmt ='%.04f', delimiter='\t')
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -30,5 +32,6 @@ ax.set_xticks(range(len(categories)))
 ax.set_yticklabels(map(artist.format,categories))
 ax.set_xticklabels(map(artist.format,categories), rotation='vertical')
 cbar = plt.colorbar(cax)
+cbar.set_label(artist.format('Jaccard similarity'))
 plt.tight_layout()
 plt.savefig('./results/jaccard-similarity-marijuana.png',dpi=300)
