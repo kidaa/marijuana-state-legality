0011f0279c7bd44fe86936832b50752ae42111c5
Michael Chary <mac389@gmail.com>
2014-06-29 16:42:33 -0400

    added visualization, results folder

--- src/legality-vs-states.py
+++ src/legality-vs-states.py
@@ -7,13 +7,28 @@ import graphics as artist
 from pprint import pprint
 from matplotlib import rcParams
 
-rcParams['text.usetext'] = True
+rcParams['text.usetex'] = True
 
 READ = 'rU'
 data = json.load(open('./data/word-occurences-by-category.json',READ))
 
 def jaccard(one,two):
-	return len(set(one) & set(two))/float(len(one) + len(two))
+	return len(set(one) & set(two))/float(len(set(one) | set(two)))
 
-similarities = [[jaccard(first_category,second_category) for first_category in data] for second_category in data]
+categories = ['legal','Medical','decriminalization','illegal']
+similarities = [[jaccard(first_category,second_category) for first_category in categories] 
+														 for second_category in categories]
 
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.imshow(similarities,interpolation='nearest',aspect='auto',cmap=plt.cm.binary,
+	vmin = 0, vmax=1)
+artist.adjust_spines(ax)
+ax.set_yticks(range(len(categories)))
+ax.set_xticks(range(len(categories)))
+
+ax.set_yticklabels(map(artist.format,categories))
+ax.set_xticklabels(map(artist.format,categories), rotation='vertical')
+cbar = plt.colorbar(cax)
+plt.tight_layout()
+plt.savefig('./results/jaccard-similarity-marijuana.png',dpi=300)
