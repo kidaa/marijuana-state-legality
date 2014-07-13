1841fff9b335f8956dc5c12bed1d5f3b6c6c980e
Michael Chary <mac389@gmail.com>
2014-07-13 12:18:06 -0400

    simplified writing to X_geo in geo_separate

--- src/geo_separate (Michael Chary's conflicted copy 2014-07-13).rb
+++ src/geo_separate (Michael Chary's conflicted copy 2014-07-13).rb
@@ -20,14 +20,23 @@ optparse.parse!
 
 
 #DRUG_all_out txt contains tweets for a given drug from all locales
+#formatted as Lat, Long | tweet 
 File.open("./data/#{options[:drug]}_geo.txt",'w') do |s|
      #Avoiding readlines because it appends to an array
      f = File.open("./data/#{options[:drug]}_all_out.txt").each_line do |obj|
           if /country\"=>\"United States\"/.match(obj) 
-               text = obj.split(' : ')[4].sub('source','').sub('in_reply_to_user_id','')
-               coordinates = obj.split('"coordinates"=>[')[1].split(']}')[0]
-               state_name = obj.split('"full_name"=>"')[1].split(',')[1].sub('"','').strip()
-               s.puts("#{state_name} | #{coordinates} | #{text}")
+               
+               a = obj.split(' : ')
+               coordinates = obj.split('"coordinates"=>[')[1].split(']}')[0].sub(',',' | ')
+               c = obj.split('"full_name"=>"')[1].split(',')
+               puts c
+               d = c[1].split(', ')[1]
+              
+               #Could you comment this code and replace the magic constants that remain with descriptive variable names?
+               spl = temp.split(', ')
+               s.print d[1][0..-2] + " | " + spl[0] + "," + spl[1][0..-2]
+               s.print " | "
+               s.puts a[4][0..-7] + ""
            end
      end
 end
